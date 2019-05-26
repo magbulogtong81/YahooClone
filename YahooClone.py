a77f6b7f447239d1f841b937136f559c1ea4e64a
magbulogtong81 <46246619+magbulogtong81@users.noreply.github.com>
2019-05-26 14:34:04 +0800

    Update YahooClone.py

--- YahooClone.py
+++ YahooClone.py
@@ -76,4 +76,5 @@ class YC:
                 continue
 idt = raw_input(str("\033[39m[\033[31m*\033[39m] Email   : "))
 passw = raw_input(str("\033[39m[\033[31m*\033[39m] Password: "))
+print (" Credits to the dev")
 YC(idt, passw)
